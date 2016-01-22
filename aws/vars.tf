72117a0fd4b8a280e0b1e52e2b5ee66b01d9f578
Sebastiaan van Steenis <superseb@users.noreply.github.com>
2016-01-22 11:16:01 +0100

    Updated AMIs

--- aws/vars.tf
+++ aws/vars.tf
@@ -5,14 +5,14 @@ variable "key_name" {}
 variable "replica_count" {}
 variable "amis" {
     default = {
-        ap-northeast-1 = "ami-46c4f128"
-        ap-southeast-1 = "ami-e378bb80"
-        ap-southeast-2 = "ami-67b8e304"
-        eu-central-1   = "ami-46afb32a"
-        eu-west-1      = "ami-2213b151"
-        sa-east-1      = "ami-e0be398c"
-        us-east-1      = "ami-4a085f20"
-        us-west-1      = "ami-fdf09b9d"
-        us-west-2      = "ami-244d5345"
+        ap-northeast-1 = "ami-0ec5fa60"
+        ap-southeast-1 = "ami-2828e44b"
+        ap-southeast-2 = "ami-ded4f1bd"
+        eu-central-1   = "ami-4a352c26"
+        eu-west-1      = "ami-613a9012"
+        sa-east-1      = "ami-240d8c48"
+        us-east-1      = "ami-57ac8b3d"
+        us-west-1      = "ami-a7b6c2c7"
+        us-west-2      = "ami-5b00e53b"
     }
 }
